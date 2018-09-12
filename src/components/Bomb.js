4ff5786638221d76277df184a340f7454a19def0
ahmad Abdulkhaliq Osman <ahmadabdulkhaliq384@gmail.com>
2018-09-12 18:35:41 +0000

    automatically backed up by learn

--- src/components/Bomb.js
+++ src/components/Bomb.js
@@ -1,1 +1,11 @@
 // Bomb Component Code Goes Here
+import react from "react"
+
+export default class Bomb extends react.Component{
+  constructor(props){
+    super()
+    this.state={
+      
+    }
+  }
+}
