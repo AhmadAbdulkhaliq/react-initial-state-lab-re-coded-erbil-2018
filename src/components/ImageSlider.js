fcc865b4c3bcfde4744f8f22773a35ddb77077d4
ahmad Abdulkhaliq Osman <ahmadabdulkhaliq384@gmail.com>
2018-09-12 18:33:12 +0000

    automatically backed up by learn

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -5,7 +5,12 @@ export default class ImageSlider extends react.Component{
   constructor(){
     super()
     this.state={
-      
+      currentSlideIndex :0
     }
+  }
+  render(){
+    return(
+      
+      )
   }
 }
